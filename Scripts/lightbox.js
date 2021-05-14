c60878039fadad0a0eacaa423a74ffab649082e8
unfotografomascuu <70489589+AndresGardea@users.noreply.github.com>
2021-05-13 20:52:47 -0600

    Add files via upload

---
+++ Scripts/lightbox.js
@@ -0,0 +1,38 @@
+function Lightbox() {
+    const zoombtn = document.querySelectorAll('.img-btn');
+    const closebtn = document.querySelectorAll('.close');
+    const containerLightbox = document.querySelector('.lightbox');
+    const containerImage = document.querySelector('.img-show');
+    const image = document.querySelectorAll('.imagen-galeria');
+    const copy = document.querySelector('.copy');
+    const imgLightbox = document.querySelector('.img-lightbox');
+
+
+    zoombtn.forEach(image => {
+        image.addEventListener('click', () => {
+            containerLightbox.classList.toggle('lightbox-move');
+            containerImage.classList.toggle('lightbox-show');
+            addImage(image.getAttribute('src'), image.getAttribute('alt'))
+        })
+    })
+
+    closebtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            containerLightbox.classList.toggle('lightbox-move');
+            containerImage.classList.toggle('lightbox-show');
+        })
+    })
+
+    const addImage = (srcImage, altImage) => {
+
+        imgLightbox.src = srcImage;
+        copy.innerHTML = altImage;
+    }
+}
+
+
+
+
+$(document).ready(function () {
+    Lightbox(Lightbox)
+});
