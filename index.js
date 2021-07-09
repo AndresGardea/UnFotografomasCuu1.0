3f910e362b00ff3efb29dc5b755cfa3e7c787c9f
unfotografomascuu <70489589+unfotografomascuu@users.noreply.github.com>
2021-07-08 18:58:10 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
     const scroll = new LocomotiveScroll({
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
-        smoothMobile: true,
         inertia: 0.75,
         getDirection: true
     });
