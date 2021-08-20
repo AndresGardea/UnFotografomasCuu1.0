03544bdc6fec9a0da22001b72e345c09274142c0
unfotografomascuu <70489589+unfotografomascuu@users.noreply.github.com>
2021-08-20 01:35:42 -0600

    Add files via upload

---
+++ Scripts/barbaConfig.js
@@ -0,0 +1,128 @@
+function delay(n) {
+    n = n || 3000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function firstVisit() {
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', { duration: 0, scaleY: 1, transformOrigin: "bottom left" });    
+    tl.to('.contenido' , {duration: 0, opacity: 1});
+    tl.to('.load-anim', { duration: .9, opacity: 1 });
+    tl.to('.loading-bar', { duration: 1, scaleX: 1, transformOrigin: "left" });
+    tl.to('ul.transition li', { duration: .6, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1, });
+    tl.to('.loading-bar', { duration: 0, scaleX: 0, transformOrigin: "left" });
+    tl.from('.anim-nav', { duration: .9, translateY: -50, opacity: 0, stagger: .1, delay: -.5 });
+    tl.from('.switch', { duration: .9, translateX: 100, opacity: 0, delay: -.1 });
+    tl.to('#txt-parallax-1', { duration: .9, translateY: -50, opacity: 1, delay: -.6 });
+    tl.to('#txt-parallax-2', { duration: .9, translateY: -60, opacity: 1, delay: -.5 });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to('ul.transition li', { duration: .6, scaleY: 1, transformOrigin: "bottom left", stagger: .2 });
+    tl.to('.load-anim', { duration: .9, opacity: 1 });
+    tl.to('.loading-bar', { duration: .8, scaleX: 1, transformOrigin: "left" });
+    tl.to('.load-anim', { duration: .9, opacity: 0 });
+    tl.to('ul.transition li', { duration: .6, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1, });
+    tl.to('.loading-bar', { duration: 0, scaleX: 0, transformOrigin: "left" });
+}
+
+function principalAnimation() {
+    var tl = gsap.timeline();
+    tl.from('.anim-nav', { duration: .9, translateY: -50, opacity: 0, stagger: .1, delay: 2.9 });
+    tl.from('.switch', { duration: .9, translateX: 100, opacity: 0, delay: -.1 });
+    tl.to('#txt-parallax-1', { duration: .9, translateY: -50, opacity: 1, delay: -.6 });
+    tl.to('#txt-parallax-2', { duration: .9, translateY: -60, opacity: 1, delay: -.5 });
+}
+
+function secondaryAnimation() {
+    var tl = gsap.timeline();
+    tl.from('#logo-2, #back', { duration: .9, translateY: -50, opacity: 0, delay: 2.9 });
+    tl.from('.NavBar-2', { duration: .9, translateY: -50, opacity: 0, delay: -.9 });
+    tl.from('.anim-gallery', { duration: 1.1, translateY: 50, opacity: 0, stagger: 0.2 });
+    tl.from('.imagen-completa', { duration: 1, opacity: 0, delay: -.7 });
+}
+
+function pageout() {
+    var tl = gsap.timeline();
+    tl.to('.text', { duration: .9, opacity: 0 })
+    tl.to('.nav-links-close', { duration: .9, translateY: -50, opacity: 0, delay: -1 });
+    tl.to('#switch', { duration: .9, translateX: 100, opacity: 0, delay: -1 });
+    tl.to(".logo", { duration: .9, translateY: -50, opacity: 0, delay: -1 });
+    tl.to(".NavBar", { duration: .9, translateY: -800, opacity: 0, delay: -1 });
+}
+
+function secondarypageout() {
+    var tl = gsap.timeline();
+    tl.to('#logo-2, #back', { duration: .9, translateY: -50, opacity: 0 });
+    tl.to('.NavBar-2', { duration: .9, translateY: -50, opacity: 0, delay: -.85 });
+    tl.to('.anim-gallery', { duration: 1, translateY: 50, opacity: 0, stagger: 0.2, delay: -.8 });
+    tl.to('.imagen-completa', { duration: .9, opacity: 0, delay: -1.4 });
+}
+
+barba.hooks.after(() => {
+    scroll.update();
+});
+
+$(function () {
+    barba.init({
+
+        sync: true,
+
+        transitions: [
+            {
+
+                async leave(data) {
+
+                    const done = this.async();
+
+                    pageout();
+
+                    secondarypageout();
+
+
+                    setTimeout(function () {
+                        pageTransition();
+                    }, 200);
+
+                    await delay(1700);
+                    done();
+
+                },
+
+                async once ()  {
+                    firstVisit();
+                    scroll();
+                },
+
+                async enter () {
+                    principalAnimation();
+                    secondaryAnimation();
+                },
+
+                async beforeEnter () {
+                    scroll.setScroll(0, 0);
+                }
+            }
+        ]
+    });
+});
+
+barba.hooks.beforeEnter(() => {
+    window.scrollTo(0, 0);
+});
+
+barba.hooks.enter(() => {
+    NavSlide();
+    BtnAnim();
+    DarkMode();
+    Lightbox();
+    Sticky();
+    addZoom();
+    loco();
+});
--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+function loco() {
+    scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+        mobile: {
+            smooth: true
+        },
+        tablet: {
+            smooth: true
+        },
+        inertia: 0.80
+    })
+};
+
+/*
+
+    let currentScrollContainer = scrollContainer.querySelector('[data-scroll-container]')
+scroll = new LocomotiveScroll({
+    el: currentScrollContainer,
     smooth: true,
     inertia: 0.80,
-})
+});
+    gsap.registerPlugin(ScrollTrigger)
 
-gsap.registerPlugin(ScrollTrigger)
 
+    scroll.on('scroll', ScrollTrigger.update)
 
-scroller.on('scroll', ScrollTrigger.update)
-
-ScrollTrigger.scrollerProxy(
-    '.container', {
+    ScrollTrigger.scrollProxy(
+        '.container', {
         scrollTop(value) {
             return arguments.length ?
-            scroller.scrollTo(value, 0, 0) :
-            scroller.scroll.instance.scroll.y
+                scroll.scrollTo(value, 0, 0) :
+                scroll.scroll.instance.scroll.y
         },
         getBoundingClientRect() {
             return {
-                left: 0, top: 0, 
+                left: 0, top: 0,
                 width: window.innerWidth,
                 height: window.innerHeight
             }
         }
     }
-)
+    )
 
 
-ScrollTrigger.create({
-    trigger: '.image-mask',
-    scroller: '.container',
-    start: 'top+=30% 50%',
-    end: 'bottom-=40% 50%',
-    animation: gsap.to('.image-mask', {backgroundSize: '125%'}),
-    scrub: 2,
-})
+    ScrollTrigger.create({
+        trigger: '.image-mask',
+        scroll: '.container',
+        start: 'top+=30% 50%',
+        end: 'bottom-=40% 50%',
+        animation: gsap.to('.image-mask', { backgroundSize: '125%' }),
+        scrub: 2,
+    })
 
 
-ScrollTrigger.addEventListener('refresh', () => scroller.update())
+    ScrollTrigger.addEventListener('refresh', () => scroll.update())
 
 
-ScrollTrigger.refresh()
+    ScrollTrigger.refresh()
+
+*/$(document).ready(function () {
+    loco(loco)
+});
